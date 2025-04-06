284409e2ae7e8daee73de2d076dc189617f90ad3
qxlt <liuqx3@gmail.com>
2025-04-06 02:19:59 -0400

    internet fixes

--- frontend/src/environment/environment.ts
+++ frontend/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendUrl: 'https://101415216-comp3133-assi2-g7d6.vercel.app/api/graphql'
+    backendUrl: 'http://localhost:4000/graphql'
 }
